310f0045ff0dd0fbd5ed275c56222853cd0896b3
Ramdas <rcoundinya@gmail.com>
2024-08-07 09:57:15 +0530

    Added delete node types

---
+++ Linked List/deleteFromTheBeginning.py
@@ -0,0 +1,33 @@
+from linkedList import Node
+from printLinkedList import printList
+
+def deleteNodeFromTheStart(head):
+    if head is not None:
+        return head.next
+    return None
+
+
+def main():
+    node1 = Node(10)
+    node2 = Node(20)
+    node3 = Node(30)
+    node4 = Node(20)
+    node5 = Node(60)
+    node6 = Node(10)
+    node7 = Node(5)
+    node8 = Node(10)
+
+    # linking Nodes
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+    node5.next = node6
+    node6.next = node7
+    node7.next = node8
+
+    node1 = deleteNodeFromTheStart(node1)
+    printList(node1)
+
+if __name__ == '__main__':
+    main()
